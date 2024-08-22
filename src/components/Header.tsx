ae489ee5155e5a7d6af94ef27da0b4fa2b285543
Victor Meireles <victoraamlima@gmail.com>
2024-08-22 14:17:02 -0300

    chore: Update Header component with lazy loading for images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
     <header className="flex justify-center py-16 px-3 relative items-center max-2md:flex-col max-2md:gap-4 max-2md:text-center">
@@ -11,19 +13,33 @@ export default function Header() {
       </div>
 
       <div className="absolute left-16 flex gap-5 max-xl:left-12 max-2md:static">
-        <a href="https://wa.link/hvz4yq" target="_blank" rel="noopener noreferrer">
-          <img
-            src="./assets/images/logo-whatsapp.png"
+        <a
+          href="https://wa.link/hvz4yq"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/assets/images/logo-whatsapp.png"
             alt="logo do whatsapp"
-            className="w-20 max-xl:w-16"
+            width={798}
+            height={802}
+            className="w-20 h-20 max-xl:w-16 max-xl:h-16"
+            priority
           />
         </a>
 
-        <a href="https://www.instagram.com/evs.shakeup/" target="_blank" rel="noopener noreferrer">
-          <img
-            src="./assets/images/logo-instagram.png"
+        <a
+          href="https://www.instagram.com/evs.shakeup/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/assets/images/logo-instagram.png"
             alt="logo do instagram"
+            width={964}
+            height={968}
             className="w-20 max-xl:w-16"
+            priority
           />
         </a>
       </div>
