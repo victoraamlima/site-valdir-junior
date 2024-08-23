4db263a2feab4ef003a113ea8cfa21f6e21383e1
Victor Meireles <victoraamlima@gmail.com>
2024-08-22 22:04:36 -0300

    chore: Update InfoIcon to use larger text size for better visibility

--- src/components/InfoIcon.tsx
+++ src/components/InfoIcon.tsx
@@ -10,7 +10,7 @@ interface InfoIconType {
 
 export default function InfoIcon({ icon }: InfoIconType) {
   return (
-    <div className="flex items-center gap-4 max-w-[50%] max-lg:max-w-full max-2md:flex-col max-2md:px-5">
+    <div className="flex items-center gap-4 max-w-[50%] max-lg:max-w-full max-2md:px-2">
       <Image
         src={icon.img}
         alt="icon"
@@ -18,7 +18,7 @@ export default function InfoIcon({ icon }: InfoIconType) {
         height={125}
         className="w-20 h-20 max-2xl:w-16 max-2xl:h-16 max-2md:w-14 max-2md:h-14 max-md:w-7 max-md:h-7"
       />
-      <p className="max-2xl:text-sm">{icon.text}</p>
+      <p className="max-2xl:text-xl">{icon.text}</p>
     </div>
   );
 }
