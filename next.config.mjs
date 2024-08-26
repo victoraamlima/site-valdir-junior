61b81bd86eb1f7522a24703a10fd93005151295a
Victor Meireles <victoraamlima@gmail.com>
2024-08-26 14:30:07 -0300

    chore: Add basePath and assetPrefix configuration for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/site-valdir-junior',
+  assetPrefix: '/site-valdir-junior/',
 };
 
 export default nextConfig;
