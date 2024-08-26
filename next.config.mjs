65bef4a043ada086dc62a2afe56a4a62a5f8685b
Victor Meireles <victoraamlima@gmail.com>
2024-08-26 17:04:58 -0300

    chore: Remove .env file from gitignore and update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/site-valdir-junior',
-  assetPrefix: '/site-valdir-junior/',
 };
 
 export default nextConfig;
